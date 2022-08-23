38b156cbd0b4a62638d0129d1342f41a494c8e18
Lucas Hideki <lucashideki@edu.unifil.br>
2022-08-23 16:40:00 -0300

    table roles

---
+++ database/migrations/2022_08_23_191556_roles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(! Schema::hasTable('clients')) {
+            Schema::create('clients', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('first_name')->nullable();
+                $table->string('last_name')->nullable();
+                $table->string('phone')->nullable();
+                $table->string('email')->nullable();
+                
+                $table->timestamps();
+                $table->softDeletes();
+
+                $table->index(['deleted_at']);
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+};
